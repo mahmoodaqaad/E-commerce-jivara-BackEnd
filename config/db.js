5061f655ef4ae9a3dc3db70c7287ec0160541c2e
mahmood aqaad <(mahmmodaqaad)>
2024-12-01 12:24:58 +0200

    Add or update .env file

--- config/db.js
+++ config/db.js
@@ -2,12 +2,13 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database,
-    port: process.env.port
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 });
+
 
 db.connect((err) => {
     if (err) {
